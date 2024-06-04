019d18c66ee486ac8bb56a3598f089c1f4457eb1
Mathew Zamacona <mateoszamacona@icloud.com>
2024-06-04 17:41:10 -0600

    syntax fix

--- index.js
+++ index.js
@@ -1,33 +1,23 @@
-
-
-
-
 document.getElementById("calculate").addEventListener("click", (e) => {
-
-
     
-const superHeroes = {
-    Tony: 42,
-  Peter: 18,
-  Steve: 104,
-  Bruce: 38,
-  Thor: 1055
-}
-
-function averageAge(superHeroes){
-    let i = 0;
-    let counter = 0;
-    let divide = 0;
-    
-    for (const [key, value] of Object.entries(superHeroes)) {
-        counter += value; 
-        divide += 1;
-    total = counter / divide; 
-    }
-    
-
-    console.log(total);    
+    const superHeroes = {
+        Tony: 42,
+        Peter: 18,
+        Steve: 104,
+        Bruce: 38,
+        Thor: 1055
+    };
+    function averageAge(superHeroes){
+        let counter = 0;
+        let divide = 0;
+        let total = 0; // Declare the total variable
+        
+        for (const [key, value] of Object.entries(superHeroes)) {
+            counter += value; 
+            divide += 1;
+        }
+        total = counter / divide;
+        console.log(total);
     }
     averageAge(superHeroes);
-}
-);
+});
